b4d2ddad9697cb8f2367eae13deebcb6410e3ee6
Mark Hickey <mark@osuhickeys.net>
2025-07-21 12:34:53 -0500

    Fixed early version check in entrypoint

--- entrypoint.go
+++ entrypoint.go
@@ -95,9 +95,10 @@ func checkIntegrity() error {
 }
 
 func generateVersionStatusFile() {
-	cmd := exec.Command("/usr/local/bin/python3", "-m", "app.services.scheduler.version_check")
+	cmd := exec.Command("/usr/local/bin/python3", "app/services/scheduler/version_check.py")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = append(os.Environ(), "PYTHONPATH=/grylli")
 	err := cmd.Run()
 	if err != nil {
 		log.Printf("[entrypoint]  Failed to write version_status.json: %v", err)
